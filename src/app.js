7165129b4d5acd3f6f01f2e36401a4a3f26e3958
Karla9191 <marietha2014@gmail.com>
2021-03-25 03:32:58 +0000

    Final

--- src/app.js
+++ src/app.js
@@ -6,17 +6,38 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
 
-  let excuseF = onLoad();
+  let action = ["ate", "peed", "crushed", "broke"];
+
+  let what = ["my homework", "the keys", "the car"];
+
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let RanWho = who[Math.floor(Math.random() * who.length)];
+
+  let RanAction = action[Math.floor(Math.random() * action.length)];
+
+  let RanWhat = what[Math.floor(Math.random() * what.length)];
+
+  let RanWhen = when[Math.floor(Math.random() * when.length)];
+
+  let totalFrase = RanWho + " " + RanAction + " " + RanWhat + " " + RanWhen;
 
   //let ejemplo = "Karla Hola Mundo";
 
   //document.getElementById("excuseF").innerHTML = ejemplo;
 
-  document.getElementById("excuseF").innerHTML = excuseF;
+  document.getElementById("excuseF").innerHTML = totalFrase;
 };
 
+/*
 function onLoad() {
   let who = ["The dog", "My grandma", "His turtle", "My bird"];
 
@@ -45,4 +66,4 @@ function onLoad() {
   console.log(totalFrase);
 
   //console.log(RanWho, RanAction, RanWhat, RanWhen);
-}
+}*/
